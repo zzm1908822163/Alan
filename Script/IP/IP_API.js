187117a8919592a1cba16583c6a2268265677241
Alan <86471714+zzm1908822163@users.noreply.github.com>
2021-08-18 14:25:42 +0800

    Update IP_API.js

--- Script/IP/IP_API.js
+++ Script/IP/IP_API.js
@@ -2,41 +2,6 @@ if ($response.statusCode != 200) {
   $done(null);
 }
 
-const emojis= ['🆘','🈲','⚠️','🔞','📵','🚦','🏖','🖥','📺','🐧','🐬','🦉','🍄','⛳️','🚴','🤑','👽','🤖','🎃', '👺', '👁', '🐶', '🐼','🐌', '👥']
-var city0 = "高谭市";
-var isp0 = "Cross-GFW.org";
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
-function City_ValidCheck(para) {
-  if(para) {
-  return para
-  } else
-  {
-  return city0
-//emojis[getRandomInt(emojis.length)]
-  }
-}
-
-function ISP_ValidCheck(para) {
-  if(para) {
-  return para
-  } else
-  {
-  return isp0
-//emojis[getRandomInt(emojis.length)]
-  }
-}
-
-function Area_check(para) {
-  if(para=="中华民国"){
-  return "台湾"
-  } else
-  {
-  return para
-  }
-}
 
 var obj = JSON.parse(body);
 var title = City_ValidCheck(obj['city']);//+Area_check(obj['country']);
