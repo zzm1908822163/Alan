39023106099d8c6c4836372c4d8f935f7986fbf1
Alan <86471714+zzm1908822163@users.noreply.github.com>
2021-08-25 15:02:45 +0800

    Update IP_API.js

--- Script/IP/IP_API.js
+++ Script/IP/IP_API.js
@@ -8,17 +8,6 @@ var isp0 = "Cross-GFW.org";
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
-
-function City_ValidCheck(para) {
-  if(para) {
-  return para
-  } else
-  {
-  return city0
-//emojis[getRandomInt(emojis.length)]
-  }
-}
-
 function ISP_ValidCheck(para) {
   if(para) {
   return para
@@ -39,7 +28,7 @@ function Area_check(para) {
 }
 var body = $response.body;
 var obj = JSON.parse(body);
-var title = City_ValidCheck(obj['city']);//+Area_check(obj['country']);
+var title = obj['city'];//+Area_check(obj['country']);
 var subtitle = ISP_ValidCheck(obj['org']);
 var ip = obj['query'];
 var description = '服务商:'+obj['isp'] + '\n'+'地区:' +City_ValidCheck(obj['regionName'])+ '\n' + 'IP:'+ obj['query'] + '\n' +'时区:'+ obj['timezone'];
