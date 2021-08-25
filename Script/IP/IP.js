72ea25b52a91488f7776615235b1bb440035f104
Alan <86471714+zzm1908822163@users.noreply.github.com>
2021-08-25 14:51:17 +0800

    Update IP.js

--- Script/IP/IP.js
+++ Script/IP/IP.js
@@ -6,7 +6,7 @@ var body = $response.body;
 var obj = JSON.parse(body);
 var title = obj['country'];
 var subtitle = obj['isp'];
-var ip = obj['query'];
-var description = "国家/地区" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "ISP" + ":" + obj['isp'] + '\n' + "AS" + ":" + obj['as'] + '\n' + "IP" + ":" + obj['ip'];
+var ip = obj['addr'];
+var description = "国家/地区" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "ISP" + ":" + obj['isp'] + '\n' + "AS" + ":" + obj['as'] + '\n' + "IP" + ":" + obj['addr'];
 
 $done({title, subtitle, ip, description});
